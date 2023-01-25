d5baef744b99fbfaf2e1d263f1d3448786dffe0d
John Cullom / cullo006 <johnccullom@gmail.com>
2023-01-25 12:21:54 -0600

    added server and templates

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
+## app.rout returns a function when "/" is called in the url, returns hello_world
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/hi', methods=['GET'])
+def hi():
+  user_name = request.args.get("userName", "unknown")
+  return render_template('main.html', user=user_name) 
